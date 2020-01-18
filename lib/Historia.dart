290dc6d541d023681d981214301011d5ddb8cfc0
Eddy <miguepoke28@gmail.com>
2020-01-18 12:19:16 -0500

    Historia dart creado y vinculado al main

---
+++ lib/Historia.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Historia extends StatefulWidget {
+  @override
+  _HistoriaState createState() => _HistoriaState();
+}
+
+class _HistoriaState extends State<Historia> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Historia')
+    );
+  }
+}
